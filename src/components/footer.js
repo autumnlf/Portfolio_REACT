ce5f5d3a9ae46c7f8bc631d0f29bf4b31ffe7315
autumnlf <autumnF12@gmail.com>
2022-05-25 03:21:56 -0400

    added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 function Footer() {
     return (
-        <Footer className="footer">
-            <div id="lineBreak"></div>
-            <div>
-                <a href="https://github.com/autumnlf" target="blank">GitHub</a>
-                <a href="http://www.linkedin.com/in/autumn-fields-2021" target="blank">LinkedIn</a>
-                <a href="https://www.autumnlfields.com/my-work" target="blank">Desgin Portfolio</a>
+        <footer>
+            <div id="contactNav">
+                <a className='nav' href="https://github.com/autumnlf" target="blank">GitHub</a>
+                <a className='nav' href="http://www.linkedin.com/in/autumn-fields-2021" target="blank">LinkedIn</a>
+                <a className='nav' href="https://www.autumnlfields.com/my-work" target="blank">Design Portfolio</a>
             </div>
-        </Footer>
+        </footer>
     );
 }
 
