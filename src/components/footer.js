0bc33cb281ebc47235b51fba7817ad727850d1d1
autumnlf <autumnF12@gmail.com>
2022-05-25 03:34:10 -0400

    added email to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,7 @@ function Footer() {
     return (
         <footer>
             <div id="contactNav">
+                <a className='nav' href="mailto:autumnf12@gmail.com" target="blank">autumnF12@gmail.com</a>
                 <a className='nav' href="https://github.com/autumnlf" target="blank">GitHub</a>
                 <a className='nav' href="http://www.linkedin.com/in/autumn-fields-2021" target="blank">LinkedIn</a>
                 <a className='nav' href="https://www.autumnlfields.com/my-work" target="blank">Design Portfolio</a>
